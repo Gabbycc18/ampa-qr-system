c5d4129642d121a575b9d42423d459e161200a3c
Gabbycc18 <gepatinomur@hotmail.com>
2026-04-28 22:27:30 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -13,17 +13,19 @@ st.markdown("""
 
 st.markdown("---")
 
-st.info("Seleccione Paciente o Médico en el menú lateral.")
+st.info("Seleccione una opción:")
 
 col1, col2 = st.columns(2)
 
 with col1:
     st.markdown("### 👤 Paciente")
     st.write("Registra tus mediciones y genera un QR para consulta.")
+    st.link_button("Ir a Paciente", "/Paciente")
 
 with col2:
     st.markdown("### 👩‍⚕️ Médico")
     st.write("Escanea el QR y analiza automáticamente los resultados.")
+    st.link_button("Ir a Médico", "/Medico")
 
 st.divider()
 st.caption("Proyecto de Salud Digital en Atención Primaria | AMPA QR System")
