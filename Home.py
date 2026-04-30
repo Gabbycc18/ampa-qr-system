21287a04e502269e5f363913d61afde3afb488cb
Gabbycc18 <gepatinomur@hotmail.com>
2026-04-30 19:52:00 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -6,24 +6,26 @@ st.markdown("""
 <div style="text-align: center;">
     <h1 style="color:#1F618D;">Sistema AMPA con QR</h1>
     <h4 style="color:gray;">
-    Digitalización del control domiciliario de la presión arterial
+        Digitalización del control domiciliario de la presión arterial
     </h4>
 </div>
 """, unsafe_allow_html=True)
 
 st.markdown("---")
 
-st.info("Seleccione Paciente o Médico en el menú lateral.")
-
 col1, col2 = st.columns(2)
 
 with col1:
     st.markdown("### 👤 Paciente")
     st.write("Registra tus mediciones y genera un QR para consulta.")
+    if st.button("Ir a Paciente", use_container_width=True):
+        st.switch_page("pages/1_Paciente.py")
 
 with col2:
     st.markdown("### 👩‍⚕️ Médico")
     st.write("Escanea el QR y analiza automáticamente los resultados.")
+    if st.button("Ir a Médico", use_container_width=True):
+        st.switch_page("pages/2_Medico.py")
 
 st.divider()
 st.caption("Proyecto de Salud Digital en Atención Primaria | AMPA QR System")
